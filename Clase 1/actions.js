25b5d2780cfbcc398639f1f83f3fff882f0c2941
Luz255181 <mira_luz_explo@hotmail.com>
2024-08-11 15:48:39 -0300

    Ejercicio Terminado

---
+++ Clase 1/actions.js
@@ -0,0 +1,71 @@
+const claro = document.getElementById('claro');
+const contraste = document.getElementById('contraste');
+const body = document.getElementById('cuerpo');
+const content = document.getElementById('formulario');
+const titulo = document.getElementById('titulo');
+const submit = document.getElementById('submit');
+const footer = document.getElementById('footer');
+const notificacion = document.getElementById('notification');
+let modoclaro = true;
+
+function modoClaro() {
+    claro.classList.add('claro');
+    claro.classList.remove('modedisable');
+    contraste.classList.add('modedisable');
+    contraste.classList.remove('dark');
+    body.classList.add('cuerpo');
+    body.classList.remove('cuerpodark');
+    content.classList.add('content');
+    content.classList.remove('contentdark');
+    titulo.classList.add('titulo');
+    titulo.classList.remove('titulodark');
+    submit.classList.add('claro');
+    submit.classList.remove('dark');
+    footer.classList.add('footerclaro');
+    footer.classList.remove('footerdark');
+    modoclaro = true;
+}
+
+function modoContraste() {
+    claro.classList.add('modedisable');
+    claro.classList.remove('claro');
+    contraste.classList.add('dark');
+    contraste.classList.remove('modedisable');
+    body.classList.add('cuerpodark');
+    body.classList.remove('cuerpo');
+    content.classList.add('contentdark');
+    content.classList.remove('content');
+    titulo.classList.add('titulodark');
+    titulo.classList.remove('titulo');
+    submit.classList.add('dark');
+    submit.classList.remove('claro');
+    footer.classList.add('footerdark');
+    footer.classList.remove('footerclaro');
+    modoclaro = false;
+}
+
+function mostrarNotificacion(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const lastname = document.getElementById('lastname').value;
+    const email = document.getElementById('email').value;
+    const birthdate = document.getElementById('birthdate').value;
+    const country = document.getElementById('country').value;
+
+    if (modoclaro) {
+        notificacion.classList.remove('oculto');
+        notificacion.classList.add('visibleclaro');
+    } else {
+        notificacion.classList.remove('oculto');
+        notificacion.classList.add('visibleoscuro');
+    }
+
+    notificacion.innerText = `Datos Enviados: \n Nombre: ${name}\n Apellido: ${lastname} \n Email: ${email} \n Fecha de Nacimiento: ${birthdate} \n País: ${country}`;
+
+    setTimeout(() => {
+        notificacion.classList.remove(modoclaro ? 'visibleclaro' : 'visibleoscuro');
+        notificacion.classList.add('oculto');
+        document.getElementById('formulario').reset();
+    }, 3000);
+}
